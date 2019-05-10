5ed8261b345e66232cb147480b6372e995606cd2
Simon Ibssa <ibssasimon@gmail.com>
2019-05-10 10:18:15 -0700

    adding my first templates

--- js/simple.js
+++ js/simple.js
@@ -15,3 +15,30 @@ examples.simple.helloWorld = function(opt_data, opt_ignored) {
 if (goog.DEBUG) {
   examples.simple.helloWorld.soyTemplateName = 'examples.simple.helloWorld';
 }
+
+
+examples.simple.helloName = function(opt_data, opt_ignored) {
+  return '' + ((! opt_data.greetingWord) ? 'Hello ' + soy.$$escapeHtml(opt_data.name) + '!' : soy.$$escapeHtml(opt_data.greetingWord) + ' ' + soy.$$escapeHtml(opt_data.name) + '!');
+};
+if (goog.DEBUG) {
+  examples.simple.helloName.soyTemplateName = 'examples.simple.helloName';
+}
+
+
+examples.simple.helloNames = function(opt_data, opt_ignored) {
+  var output = examples.simple.helloName(opt_data) + '<br>';
+  var additionalNameList18 = opt_data.additionalNames;
+  var additionalNameListLen18 = additionalNameList18.length;
+  if (additionalNameListLen18 > 0) {
+    for (var additionalNameIndex18 = 0; additionalNameIndex18 < additionalNameListLen18; additionalNameIndex18++) {
+      var additionalNameData18 = additionalNameList18[additionalNameIndex18];
+      output += examples.simple.helloName({name: additionalNameData18}) + ((! (additionalNameIndex18 == additionalNameListLen18 - 1)) ? '<br>' : '');
+    }
+  } else {
+    output += 'No additional people to greet.';
+  }
+  return output;
+};
+if (goog.DEBUG) {
+  examples.simple.helloNames.soyTemplateName = 'examples.simple.helloNames';
+}
